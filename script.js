68a9a6e444506b11bb157d249dfdce9465cd64fc
Caliza <rances.rodriguez@gmail.com>
2023-01-08 20:10:32 -0800

    styled answer buttons and initials log button. Added two more questions to the quiz for a total of five.

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ var questions = [
         answer: "Alerts"
     },
     {
-        title: "________ is commonly used to end a line of code.",
+        title: "________ is commonly used to end a line of code?",
         choices: ["Comma", "Semicolon", "Parentheses", "Period"],
         answer: "Semicolon"
     },
     {
-        title: "Commonly used data types do not include?",
-        choices: ["Strings", "Booleans", "Alerts", "Nubers"],
-        answer: "Alerts"
+        title: "A function is prefaced with the word?",
+        choices: ["Function", "Object", "Method", "Index"],
+        answer: "Function"
+    },
+    {
+        title: "An array is defined by?",
+        choices: ["()", "{}", "||", "[]"],
+        answer: "[]"
+    },
+    {
+        title: "A global variable is declared ________ a function",
+        choices: ["In HTML", "In CSS", "Outside", "Insude"],
+        answer: "Outside"
     }
+    
 ];
 
 var timer;
@@ -27,11 +38,13 @@ var startBtn = document.querySelector("#start-btn");
 var containerEl = document.querySelector(".container");
 var questionEl = document.querySelector(".question");
 var answersEl = document.querySelector("#answers");
-
+var ulEl = document.querySelector("#ul");
 var interval;
-
-var counter = 10;
+var scoreEl = document.querySelector("#high-score");
+var counter = 60;
 var id = document.getElementById("count");
+
+// scoreEl.classList.add("hide")
 
 startBtn.addEventListener("click", function () {
     startEl.classList.add("hide")
@@ -56,6 +69,7 @@ function showQuestion() {
     answersEl.innerHTML = "";
     for (let index = 0; index < questions[currentIndex].choices.length; index++) {
         let choiceBtn = document.createElement("button")
+        choiceBtn.className += "btn-style";
         answersEl.appendChild(choiceBtn);
         choiceBtn.innerHTML = questions[currentIndex].choices[index];
 
@@ -68,7 +82,7 @@ function showQuestion() {
 function selectedAnswer(e) {
     var selectedBtn = e.target
     if (this.value !== questions[currentIndex].answer) {
-        counter -= 2;
+        counter -= 12;
         id.innerHTML = counter;
         if (counter <= 0) {
             counter = 0;
@@ -88,10 +102,29 @@ function selectedAnswer(e) {
 
 function endQuiz(){
     clearInterval(interval)
+    
     containerEl.classList.add("hide")
     document.getElementById("end-quiz").classList.remove("hide")
     document.getElementById("score").textContent = counter;
-    document.getElementById("log-button").addEventListener("click", function(){
+    // line 95??
+    //document.getElementById("initials").textContent = initials;
+    var highScores = JSON.parse(localStorage.getItem("initials")) || []
+    console.log(highScores);
+    for (let index = 0; index < highScores.length; index++) {
+        const element = highScores[index];
+        var name =document.createElement("li");
+        name.textContent += element.initials+ " " + element.score;
+        ulEl.appendChild(name);
+    }
+}
+document.getElementById("log-button").addEventListener("click", function(){
+    var highScores = JSON.parse(localStorage.getItem("initials")) || []
+    let initials = {
+        initials: document.getElementById("initials").value,
+        score: counter
+
+    }  
+    highScores.push(initials) 
+    localStorage.setItem("initials", JSON.stringify(highScores))
     //save to local storage    "loacalStorage.setItem" make object out of score pull initial out of input box. Array of scores
     });
-}
